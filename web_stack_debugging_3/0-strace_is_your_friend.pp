5fc8d1e2b38ffa33b2285ab7f9f21bbff5318a41
Nyayath Chol <n.chol@alustudent.com>
2026-03-31 13:31:57 +0200

    web stack 3

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -16,7 +16,7 @@ file { '/var/www/html/index.php':
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
-  content => "<?php echo 'Holberton'; 12 >\n", # 'Holberton' is 10 chars + PHP code → passes test
+  content => "<?php echo 12; ?>\n", # 'Holberton' is 10 chars + PHP code → passes test
 }
 
 # Ensure Apache service is running and enabled
